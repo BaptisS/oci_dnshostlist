6058b4c3898253764448d15d069d710422871a29
BaptisS <56864846+BaptisS@users.noreply.github.com>
2020-07-10 11:47:03 +0200

    Create vcnhostlist.sh

---
+++ vcnhostlist.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#compocid=
+#vcnid=
+
+rm -f pvtip.sh
+wget https://raw.githubusercontent.com/BaptisS/oci_dnshostlist/master/pvtip.sh
+chmod +x pvtip.sh 
+
+vcnsub=$(oci network subnet list --compartment-id $2 --vcn-id $1)
+
+sublist=$(echo $vcnsub | jq -r '.data[] | ."id"') 
+for sub in $sublist; do ./pvtip.sh $sub ; done
